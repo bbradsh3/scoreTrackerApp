b8c112997056390dae9d614b4e8abe6a2d752167
Brandon Bradshaw <bbradsh3@uccs.edu>
2023-04-24 18:33:43 +0000

    added validation tests for creating and editing rounds

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,2 +1,16 @@
 class Round < ApplicationRecord
+    #ensure that all information is provided when creating a new round
+    validates_presence_of :course
+    validates_presence_of :weather
+    validates_presence_of :date
+    validates_presence_of :hole1
+    validates_presence_of :hole2
+    validates_presence_of :hole3
+    validates_presence_of :hole4
+    validates_presence_of :hole5
+    validates_presence_of :hole6
+    validates_presence_of :hole7
+    validates_presence_of :hole8
+    validates_presence_of :hole9
+
 end
---
+++ spec/models/project_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe "Round Attribute Requirements on Create", :type => :model do
+  context "validation tests" do
+    it "ensures the course is present when creating project" do
+      round = Round.new(weather: "sunny", date: "April 24, 2023", hole1: "0", hole2: "0", hole3: "0", hole4: "0", hole5: "0", hole6: "0", hole7: "0", hole8: "0", hole9: "0")
+      expect(round.valid?).to eq(false)
+    end
+    it "should not be able to save round when score is missing" do
+      round = Round.new(course: "Cottonwood", weather: "sunny", date: "April 24, 2023", hole1: "0", hole2: "0", hole3: "0", hole4: "0", hole5: "0", hole6: "0", hole7: "0", hole8: "0")
+      expect(round.save).to eq(false)
+    end
+    it "should be able to save round when " do
+      round = Round.new(course: "Cottonwood", weather: "sunny", date: "April 24, 2023", hole1: "0", hole2: "0", hole3: "0", hole4: "0", hole5: "0", hole6: "0", hole7: "0", hole8: "0", hole9: "0")
+      expect(round.save).to eq(true)
+    end
+  end
+end
+
+describe "Round Attribute Requirements on Edit", :type => :model do
+  context "Edit round" do  
+    before (:each) do
+      @round = Round.create(course: "Cottonwood", weather: "sunny", date: "April 24, 2023", hole1: "0", hole2: "0", hole3: "0", hole4: "0", hole5: "0", hole6: "0", hole7: "0", hole8: "0", hole9: "0")
+ 
+      end
+    it "ensures the course is present when editing project" do
+      @round.update(:course => "New Course")
+      expect(@round.course == "New Course")
+    end
+    it "ensures the weather is present when editing project" do
+        @round.update(:weather => "")
+        expect(@round.save).to eq(false)
+  end
+end
+end
